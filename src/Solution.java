fcf17f0333d2b3125003e1e890e02664f261aaf6
JorgeAgraso <jagrasoreyes@gmail.com>
2025-03-17 19:21:23 +0000

    Primer Commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.lang.StringBuilder;
 
-public class Solution {
+/**
+ * La clase Solution contiene el método toCamelCase
+ *
+ * @author JorgeAgrasoReyes
+ *
+ * @version 17/03/2025
+ */
+class Solution{
+
+    /**
+     * Este método recibe una String(ej: the-stealth-warrior, ej2: The_Stealth_Warrior) y devuelve una String
+     * de forma camelCase(ej: tHeStealthWarrior, ej2: TheStealthWarrior).
+     *
+     * @param s Recibe un parámtro de tipo String.
+     *
+     * @return Devuelve un parámetro de tipo String.
+     */
+
+    static String toCamelCase(String s){
+        return "";
+    }
 }
